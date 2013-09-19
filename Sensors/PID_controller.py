77b20de0d119dfb8ea6185a7c1292565495e68f6
sloev <johannesgj@gmail.com>
2013-09-19 14:05:21 +0200

    added calc object

--- Sensors/PID_controller.py
+++ Sensors/PID_controller.py
@@ -2,21 +2,29 @@
 Created on Sep 11, 2013
 
 @author: johannes
+@review: johannes, benjamin
 '''
 import MouseInput
+from CalculateAngle import Calculations
+
 
 class MyClass(object):
     '''
     classdocs
     '''
     miceSensors = MouseInput()
+    calculator=Calculations()
 
     def __init__(self):
+        global calculator
         '''
         Constructor
         '''
         
         #miceSensors.update()
+        angLen=calculator.calcAngle()
+        angle=angLen()[0]
+        length=angLen()[1]
         pass
     
     def computeAngle(self):
