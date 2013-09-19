74937345196fc844222d5c9d1eb490909d353c5f
sloev <johannesgj@gmail.com>
2013-09-19 14:05:03 +0200

    review by

--- Sensors/LookUpTable.py
+++ Sensors/LookUpTable.py
@@ -21,42 +21,42 @@ class LookUpTable:
     
 
     def __init__(self):
-        initCosTable()
-        initAngLenTable()
+        self.initCosTable()
+        self.initAngLenTable()
         
-        def initCosTable(self):
-            global cosTable
-            for i in range (0,511):
-                cosTable[i]=cos(i*(math.pi/512))
+    def initCosTable(self):
+        global cosTable
+        for i in range (0,511):
+            cosTable[i]=cos(i*(math.pi/512))
             
-        def initAngLenTable(self):
-            global angLenTable
+    def initAngLenTable(self):
+        global angLenTable
 #            entries = 0
 
-            for y in range(0, 255):
-                newy = y-128
-                for x in range(0, 255):
-                    newx = x-128
-                    if(newx!=0 and newy!=0):
-                        angle = float(math.atan(Decimal(newy)/Decimal(newx)))     
-                        if(angle==0 or angle==math.pi):
-                            length=math.fabs(newx)
-                        else:                          
-                            length = math.fabs((newy)/(math.sin(angle)))
-                            #angLenTable[x][y] = Mouse(newx,newy,angle,length)
-                            angLenTable[x][y] = angle,length
+        for y in range(0, 255):
+            newy = y-128
+            for x in range(0, 255):
+                newx = x-128
+                if(newx!=0 and newy!=0):
+                    angle = float(math.atan(Decimal(newy)/Decimal(newx)))     
+                    if(angle==0 or angle==math.pi):
+                        length=math.fabs(newx)
+                    else:                          
+                        length = math.fabs((newy)/(math.sin(angle)))
+                        #angLenTable[x][y] = Mouse(newx,newy,angle,length)
+                        angLenTable[x][y] = angle,length
         
-        def getCos(angle):
-            index=round(angle*(1/(math.pi/512)))
-            return cosTable[index]
+    def getCos(self,angle):
+        index=round(angle*(1/(math.pi/512)))
+        return cosTable[index]
         
-        def getAngLen(x,y):
-            return angLenTable[x][y]
+    def getAngLen(self,x,y):
+        return angLenTable[x][y]
         
-        def printAngLenTable(self):
-            for y in range (0,255):
-                for x in range(0,255):
-                    print(angLenTable[x][y].toString())
+    def printAngLenTable(self):
+        for y in range (0,255):
+            for x in range(0,255):
+                print(angLenTable[x][y].toString())
 
         
 
