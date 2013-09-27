0300de630d9b5091e4446e79957fb8a56d589607
sloev <johannesgj@gmail.com>
2013-09-27 02:55:05 -0700

    class

--- Motor_control/Motor_I2C.py
+++ Motor_control/Motor_I2C.py
@@ -220,7 +220,7 @@ def main():
         position+=i*1000
         motor2.setMotorParam(1,(i%5)+1,2)
         returner=motor2.getFullstatus2()
-        print(returner+"\tclass="+returner.__class__.__name__)
+        print(str(returner)+"\tclass="+str(returner.__class__.__name__))
         
 #        motor1.setPosition(position)
         time.sleep(2)
