496b6000ec8f00fdcad21d7d2c2a04c82cf2eaad
sloev <johannesgj@gmail.com>
2013-09-27 02:37:12 -0700

    kun vel

--- Motor_control/Motor_I2C.py
+++ Motor_control/Motor_I2C.py
@@ -211,16 +211,15 @@ def main():
     #time.sleep(2)
     position=10000
     motor1.setPosition(position)
+    position=40000
     motor2.setPosition(position)
     time.sleep(5)
     
     for i in range(0,100):
         i+=1
-        position+=i*100
         motor2.setMotorParam(1,(i%5)+1,2)
         
 #        motor1.setPosition(position)
-        motor2.setPosition(position)
         time.sleep(1)
     
     
