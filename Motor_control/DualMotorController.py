77bcfc6b5f3930fcc21de44a94f1410618ebba9c
sloev <johannesgj@gmail.com>
2013-10-02 07:49:25 -0700

    lol

--- Motor_control/DualMotorController.py
+++ Motor_control/DualMotorController.py
@@ -126,7 +126,9 @@ def main():
     time.sleep(3)
      
     print("turning 180")
-    dualMotors.turnAround()
+    dualMotors.turnLeft()
+    dualMotors.turnLeft()
+
 #     while 1:
 #         tmp=dualMotors.getActPosTarPosMatrix()
 #         if(tmp[0][0]==tmp[0][1] and tmp[1][0]==tmp[1][1]):
