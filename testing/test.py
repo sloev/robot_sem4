9222e4656e210112ad5072f76f35e347aa8a68dc
sloev <johannesgj@gmail.com>
2013-10-04 03:08:49 -0700

    lol

---
+++ testing/test.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 4, 2013
+
+@author: slavegnuen
+'''
+from Decorators.TMC222Status import TMC222Status
+def main():
+    print("success2")
+    
+if __name__ == '__main__':
+    pass
