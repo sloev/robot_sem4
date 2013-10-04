1e51979325f8fb070322478014ec3f4744d96ad2
sloev <johannesgj@gmail.com>
2013-10-04 03:25:09 -0700

    lol

--- lib/testing/test.py
+++ lib/testing/test.py
@@ -3,7 +3,7 @@ Created on Oct 4, 2013
 
 @author: slavegnuen
 '''
-from Decorators.TMC222Status import getFullStatus1
+from lib.Decorators.TMC222Status import getFullStatus1
 
 def main():
     print("success2")
