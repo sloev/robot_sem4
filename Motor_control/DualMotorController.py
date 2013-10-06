76f8f1017a1677b1d7bdae9aafe52783354d827a
sloev <johannesgj@gmail.com>
2013-10-06 02:50:41 -0700

    lols

--- Motor_control/DualMotorController.py
+++ Motor_control/DualMotorController.py
@@ -3,152 +3,73 @@ Created on Oct 2, 2013
 
 @author: johannes, benjamin
 '''
+'class variables:'
+turn90Steps=2000
+turn180Steps=4000
 
 from Motor_I2C import Motor_I2C
 import time as time
 
-'in full steps'
-halfRevolution=135 
-quarterRevolution=67
-
 class DualMotorController:
     def __init__(self, add1, add2):
-        self.left = Motor_I2C(add1)
-        self.right = Motor_I2C(add2)
-        self.stepsPrStep=8
-        self.posLeft=201
-        self.posRight=201
-        self.targetPosition=400
-        
-        
-    def dualSetOTPParam(self):
-        self.left.setOTPParam()
-        time.sleep(0.1)
-        self.right.setOTPParam()
-        
-    def dualResettoDefault(self):
-        self.left.resetToDefault()
-        self.right.resetToDefault()
+        self.motorLeft = Motor_I2C(add1)
+        self.motorRight = Motor_I2C(add2)
+        self.positionLeft=200
+        self.positionRight=200
     
-    def busy(self):
-        leftstatus=self.left.getFullStatus2()
-        rightstatus=self.right.getFullStatus2()
-        leftstatus=(leftstatus[1]<<8 | leftstatus[2]<<0) | (leftstatus[3]<<8 | leftstatus[4]<<0)
-        rightstatus=(rightstatus[1]<<8 | rightstatus[2]<<0) | (rightstatus[3]<<8 | rightstatus[4]<<0)
-        return (leftstatus & rightstatus)==0
-    
-    def turnAround(self):
-        self.turn90(1, 2)
-    
-    def turnLeft(self):
-        self.turn90(1,1)
-    
-    def turnRight(self):
-        self.turn90(0,1)
+    def setOtpParam(self):
+        self.motorLeft.setOTPParam()
+        self.motorRight.setOTPParam()
         
-    def turn90(self,direction,times):
-        self.dualSetDirection(direction)
-        self.dualUpdateMotorParams()
+    def runInit(self):
+        self.motorLeft.runInit()
+        self.motorRight.runInit()
+    
+    def setMotorParams(self,leftDir,rightDir,leftMaxVel,rightMaxVel):
+        self.motorLeft.setMotorParam(leftDir, leftMaxVel)
+        self.motorRight.setMotorParam(rightDir, rightMaxVel)
+    
+    def getFullStatus1(self):
+        return [self.motorLeft.getFullStatus1(),self.motorLeft.getFullStatus1()]
+    
+    def getFullStatus2(self):
+        return [self.motorLeft.getFullStatus2(),self.motorLeft.getFullStatus2()]
+    
+    def turn90(self,direction,maxVel):
+        self.motorLeft.setMotorParam(direction, maxVel)
+        self.motorRight.setMotorParam(not direction, maxVel)
         
-        incpos=(quarterRevolution*self.stepsPrStep)*times
-        self.dualIncTargetPosition(incpos)
+        self.setPosition(turn90Steps, turn90Steps)
+        
+    def turn180(self,maxVel):
+        self.motorLeft.setMotorParam(dir, maxVel)
+        self.motorRight.setMotorParam(not dir, maxVel)
+        
+        self.setPosition(turn180Steps, turn180Steps)
+        
+    def setPosition(self,incLeftPos,incRightPos):
+        self.positionLeft+=incLeftPos
+        self.positionRight+=incRightPos
+        
+        self.motorLeft.setPosition(self.positionLeft)
+        self.motorLeft.setPosition(self.positionRight)
+    
+    def isBusy(self,fullStatus2Matrix):
+        leftstatus=fullStatus2Matrix[0][:]
+        rightstatus=fullStatus2Matrix[1][:]
+        leftstatus=(leftstatus[1]<<8 | leftstatus[2]<<0) & (leftstatus[3]<<8 | leftstatus[4]<<0)
+        rightstatus=(rightstatus[1]<<8 | rightstatus[2]<<0) & (rightstatus[3]<<8 | rightstatus[4]<<0)
+        return (leftstatus & rightstatus)==1
+        
+    def hardStop(self):
+        self.motorLeft.hardStop()
+        self.motorRight.hardStop()
+        
+        
+        
+        
 
-    def runInit(self,posA,posB):
-        self.left.runInit(posA, posB)
-        time.sleep(0.1)
-        self.right.runInit(posA, posB)
-    
-    def getActPosTarPosMatrix(self):
-        leftstatus = self.left.getFullStatus2()
-        rightstatus = self.right.getFullStatus2()
-        leftActPos = leftstatus[1]<<8 | leftstatus[2]<<0
-        leftTarPos= leftstatus[3]<<8 | leftstatus[4]<<0
-        rightActPos = rightstatus[1]<<8 | rightstatus[2]<<0
-        rightTarPos = rightstatus[3]<<8 | rightstatus[4]<<0
-        return [[leftActPos,leftTarPos],[rightActPos,rightTarPos]]
-    
-    def dualIncTargetPosition(self,incPosition):   
-        self.targetPosition+=incPosition     
-        self.left.setPosition(self.targetPosition)
-        self.right.setPosition(self.targetPosition)
-    
-    def dualUpdateMotorParams(self):
-        self.left.setMotorParam()
-        self.right.setMotorParam()
-        
-    def dualHardstop(self):
-        self.left.hardStop()
-        self.right.hardStop()
-    
-    def dualSoftstop(self):
-        self.left.softStop()
-        self.right.softStop()
-        
-    'set motor values:'
-    
-    def dualSetIrun(self,irun):
-        self.left.setIrun(irun)
-        self.right.setIrun(irun)
-        
-    def setLeftMaxVel(self,maxVelocity):
-        self.left.setMaxVelocity(maxVelocity)
-        
-    def setRightMaxVel(self,maxVelocity):
-        self.right.setMaxVelocity(maxVelocity)
-        
-    def dualSetMaxVel(self,maxVelocity):
-        self.setLeftMaxVel(maxVelocity)
-        self.setRightMaxVel(maxVelocity)
-        
-    def setLeftDirection(self,direction):
-        self.left.setDirection(direction)
- 
-    def setRightDirection(self,direction):
-        self.right.setDirection(direction)
-    
-    def dualSetDirection(self,direction):
-        self.setRightDirection(direction)
-        self.setLeftDirection(direction)
-        
-    def leftGetFullstatus1(self):
-        r=self.left.getFullStatus1()
-        return r
-        
-    def rightGetFullstatus1(self):
-        r=self.right.getFullStatus1()
-        return r
-     
-def main():
-    print("create motor instances")
-    dualMotors=DualMotorController(0x60,0x61)
-    print("current positions (act/tar/act/tar):"+str(dualMotors.getActPosTarPosMatrix()))
-    dualMotors.dualSetOTPParam()
-    dualMotors.dualSetIrun(5)
-    dualMotors.dualSetMaxVel(5)
-    dualMotors.setLeftDirection(1)
-    dualMotors.setRightDirection(0)
-    dualMotors.dualUpdateMotorParams()
-    
-    print("running init")
-    dualMotors.runInit(1000, 3000)
-    #dualMotors.dualIncTargetPosition(3000)
-   
-#     for i in range(1,30):
-#         index=(i%6)+1
-#         dualMotors.dualSetMaxVel(index)
-# 
-#         dualMotors.dualUpdateMotorParams()
-#         dualMotors.dualIncTargetPosition(4000)
-# #        print("IRun is="+str(i)+" current positions (act/tar/act/tar):"+str(dualMotors.getActPosTarPosMatrix()))
-#         time.sleep(1)
-#     print("IRun is="+str(i)+" current positions (act/tar/act/tar):"+str(dualMotors.getActPosTarPosMatrix()))
 
-    print(dualMotors.leftGetFullstatus1())
-    time.sleep(0.1)
-    print(dualMotors.rightGetFullstatus1())
-        
-        
-        
      
 if __name__ == '__main__':
     main()
