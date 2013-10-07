4559e468918830ebd08b5bf97a56164899c420d9
sloev <johannesgj@gmail.com>
2013-10-07 06:31:31 -0700

    hahaha

--- Motor_control/DualMotorController.py
+++ Motor_control/DualMotorController.py
@@ -78,7 +78,6 @@ class DualMotorController:
     def softStop(self):
         self.motorLeft.softStop()
         self.motorRight.softStop()
-        git tag -a v0.1 -m 'working dualmotors'
         
 def main():
     print("init")
