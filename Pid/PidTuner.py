775b867778e1c4a5e944a6969ff8bea509ea240d
sloev <johannesgj@gmail.com>
2013-10-25 14:11:56 +0200

    test

--- Pid/PidTuner.py
+++ Pid/PidTuner.py
@@ -139,7 +139,7 @@ def main():
         # get keyboard input, returns -1 if none available
         while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
             c = sys.stdin.readline()
-            print("c is ="+c)
+            print("c is ="+c+"class="+c.__name__)
             if(c=='a'): 
                 pidtuner.lpgadd()
                 print("left pgain inc")
