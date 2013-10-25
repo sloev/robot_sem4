94c5ec532bf3b918f7266bdd806c44a8390ce414
sloev <johannesgj@gmail.com>
2013-10-25 14:29:57 +0200

    lol

--- Pid/Pid.py
+++ Pid/Pid.py
@@ -33,7 +33,7 @@ class Pid():
         self.ir_sensors=ir_sensors
         self.dual_motors=dual_motors
         self.setPoint=10
-        self.cmMax=20
+        self.cmMax=40
         self.cmMin=5
         
         self.lastError=[0,0] #last error 
