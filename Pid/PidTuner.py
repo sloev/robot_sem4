c9cc67895057a22794ff24d02fcc6eeea4bfe9d8
sloev <johannesgj@gmail.com>
2013-10-28 15:22:57 +0100

    test

--- Pid/PidTuner.py
+++ Pid/PidTuner.py
@@ -123,7 +123,7 @@ class PidTuner():
             
     def turn(self,direction):
         print("turning wheel="+str(direction))
-        time.sleep(0.8)
+        time.sleep(1.2)
         self.dual_motors.softStop()
         time.sleep(0.3)
         self.dual_motors.turn90(direction, 2)
