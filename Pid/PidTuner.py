9f61d2b5875d2e554e54051276d71ed95e63e126
sloev <johannesgj@gmail.com>
2013-10-28 14:10:01 +0100

    lol

--- Pid/PidTuner.py
+++ Pid/PidTuner.py
@@ -109,6 +109,7 @@ class PidTuner():
     def doPid(self):
         try:
             self.printGains()
+            self.dual_motors.setMotorParams(self.left, self.right, 2, 2)
             self.dual_motors.setPosition(32767, 32767)
             walls=self.pid.doPid()
             print("[walls="+str(walls)+"]")
