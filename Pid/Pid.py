51c688071b44f4715597f29ac6f347e68142c9fe
sloev <johannesgj@gmail.com>
2013-10-28 14:21:17 +0100

    lol

--- Pid/Pid.py
+++ Pid/Pid.py
@@ -164,14 +164,14 @@ class Pid():
         cm=self.constrain(cm)
         print("soft cm="+str(cm))
         value=2
-        if(cm > 0):
-            if(cm >0 and cm < 1):
+        if(cm > 0.5):
+            if(cm >0.5 and cm < 2):
                 value=3
-            elif(cm > 1 and cm < 2):
-                value=4  
             elif(cm > 2 and cm < 4):
+                value=4  
+            elif(cm > 4 and cm < 8):
                 value=5 
-            elif(cm >4 and cm < 10):
+            elif(cm >8 and cm < 10):
                 value=6            
         return value
     
