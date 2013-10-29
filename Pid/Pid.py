ea13cd0d79476ce1ed5f95e1d5996db47e3009e8
sloev <johannesgj@gmail.com>
2013-10-29 13:18:55 +0100

    lol

--- Pid/Pid.py
+++ Pid/Pid.py
@@ -39,8 +39,8 @@ class Pid():
         self.logger.info("Initializing Pid")
         self.ir_sensors=ir_sensors
         self.dual_motors=dual_motors
-        self.setPoint=13
-        self.cmMax=28
+        self.setPoint=15
+        self.cmMax=32
         self.cmMin=5
         
         self.lastError=[0,0] #last error 
