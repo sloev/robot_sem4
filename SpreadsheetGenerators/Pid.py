190b26b487a94aa730f3779f5b517cdaed42d2a2
sloev <johannesgj@gmail.com>
2013-10-30 23:07:00 +0100

    lol

--- SpreadsheetGenerators/Pid.py
+++ SpreadsheetGenerators/Pid.py
@@ -14,7 +14,6 @@ class Pid():
         self.startTime= datetime.fromtimestamp(0)
 
         self.patterns=[
-                       '/robot',
                        'left/pErrorWithGain/',
                        'right/pErrorWithGain/',
                        'left/iErrorWithGain/',
@@ -36,23 +35,22 @@ class Pid():
         self.allOut="leftPGE\trightPGE\tleftIGE\trightIGE\tleftDGE\trightDGE\tleftValCm\trightValCm\tleftValVel\trightValVel\tleftPE\trightPE\tleftIE\trightIE\tleftDE\trightDE\n"
 
         self.functions=[
-                        self.timecode,
-                        self.leftPGE,
-                        self.rightPGE,
-                        self.leftIGE,
-                        self.rightIGE,
-                        self.leftDGE,
-                        self.rightDGE,
-                        self.leftValCm,
-                        self.rightValCm,
-                        self.leftValVel,
-                        self.rightValVel,
-                        self.leftPError,
-                        self.rightPError,
-                        self.leftIError,
-                        self.rightIError,
-                        self.leftDError,
-                        self.rightDError
+                        self.start,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.same,
+                        self.finnish
                         ]
         
     def __call__(self):
@@ -66,72 +64,29 @@ class Pid():
             foo.write(self.allOut)
         finally:
             foo.close    
-    def timecode(self,found):
+    def start(self,found):
         if found: 
             index=found.span()[0]
             thisDate=self.currentString[:index]
-            parts = thisDate.split(',')
-            dt = datetime.strptime(parts[0], "%Y-%m-%d %H:%M:%S")
-            dt.replace(microsecond=int(parts[1]))
+            dt = datetime.strptime(thisDate, "%Y-%m-%d %H:%M:%S,%f")
                         
             if(round(int(self.startTime.strftime('%s')),3)<1):
                 self.startTime=dt
                 currentTime=str(0)
             else:
-                currentTime=str((dt-self.startTime).strftime('%s.%f'))
+                delta=(dt-self.startTime).total_seconds()
+                seconds=round(delta,3)
+                currentTime=str(seconds)
             print(currentTime)
-            self.allOut=''.join([self.allOut,currentTime]) 
+            
+            index=found.span()[1]
+            perror=self.currentString[index:len(self.currentString)-2]    
+                        
+            self.allOut=''.join([self.allOut,currentTime+"\t"+perror]) 
         else: 
             pass          
 
-    def leftPGE(self,found):
-        self.same(found)
-        
-    def rightPGE(self,found):
-        self.same(found)
-
-    def leftIGE(self,found):
-        self.same(found)
-
-    def rightIGE(self,found):
-        self.same(found)
-
-    def leftDGE(self,found):
-        self.same(found)
-
-    def rightDGE(self,found):
-        self.same(found)
-
-    def leftValCm(self,found):
-        self.same(found)
-
-    def rightValCm(self,found):
-        self.same(found)
-
-    def leftValVel(self,found):
-        self.same(found)
-
-    def rightValVel(self,found):
-        self.same(found)
-        
-    def leftPError(self,found):
-        self.same(found)
-
-        
-    def rightPError(self,found):
-        self.same(found)
-
-    def leftIError(self,found):
-        self.same(found)
-
-        
-    def rightIError(self,found):
-        self.same(found)
-
-    def leftDError(self,found):
-        self.same(found)
-        
-    def rightDError(self,found):
+    def finnish(self,found):
         if found: 
             index=found.span()[1]
             perror=self.currentString[index:len(self.currentString)-2]
@@ -153,108 +108,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-#     
-#      def rightPGE(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def leftIGE(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def rightIGE(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def leftDGE(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def rightDGE(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def leftValCm(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def rightValCm(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def leftValVel(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     def rightValVel(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#         
-#     def leftPError(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#         
-#     def rightPError(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#         
-#     def leftIError(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#         
-#     def rightIError(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass    
-#         
-#     def leftDError(self,found):
-#         if found: 
-#             index=found.span()[1]
-#             perror=self.currentString[index:len(self.currentString)-2]                
-#             self.allOut='\t'.join([self.allOut,perror]) 
-#         else: 
-#             pass
-#     
