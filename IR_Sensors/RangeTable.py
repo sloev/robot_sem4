04f2c9c7f778189cbad5c07be4d58d262e33552f
danielmachon <danielmachon@live.dk>
2013-10-31 20:20:36 +0100

    Oprydning - Cause we like pretty things

--- IR_Sensors/RangeTable.py
+++ IR_Sensors/RangeTable.py
@@ -1,8 +1,16 @@
 '''
 Created on Sep 16, 2013
 
-@author: johannes, ivo, daniel
+@author: Johannes, Ivo, Daniel
 '''
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'This class creates a conversion lookup table from the Sharp IR ADC values,    '
+'and converts them to centimeters using an approximated linear equation,       '
+'based on measurements.                                                        '    
+'                                                                              '
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''    
+
 
 import cPickle as pickle
 import math
@@ -10,25 +18,28 @@ import os.path
 
 
 class RangeTable():
-    '''
-    creates a a conversion ookup table from sharp ir adc values to centimeters 
-    using an approximated linear equation based on mesurements
-    
-    adcmax sAEtter hvilken spEndevide vi kan opleve adc outputted i
-    '''
 
+    '''
+        Constructor
+    '''
     def __init__(self):
         self.adcMax=3000
         self.lookupTable = []
         self.initLookupTable()
         self.pickleTable()
         
+    '''
+        Create a lookup table for all possible distances
+    '''
     def initLookupTable(self):
         self.lookupTable = []
         for i in range (0,self.adcMax):
             self.lookupTable.extend([self.calcAdcToCm(i)])
             
-        '''praecis conversion skal indsaettes'''
+       
+    '''
+        Convert the ADC input to centimeters
+    '''        
     def calcAdcToCm(self,adc):
         a =       46.25  ;
         b =   -0.004601  ;
@@ -38,14 +49,25 @@ class RangeTable():
         cm = a*math.exp(b*adc) + c*math.exp(d*adc)
         return cm
             
+    
+    '''
+        Perform a table lookup
+    '''
     def lookUpDistance(self,adc):
         if(adc>0 and adc <self.adcMax):
             return self.lookupTable[adc]
         return -1
     
+    '''
+        Serialize the lookup table
+    '''
     def pickleTable(self):
         pickle.dump(self, open("rangeTable.p", "wb"), protocol=-1)
         
+    
+    '''
+        Deseriallize lookup table
+    '''
     @staticmethod
     def unpickleTable():
         returnValue=0
@@ -55,6 +77,7 @@ class RangeTable():
             except EOFError:
                 print "Error"
         return returnValue
+
 
 def main():
     LUT = RangeTable.unpickleTable()
