0bdb4c115319dfd121583d89b455e53c6180c532
sloev <johannesgj@gmail.com>
2013-11-01 16:28:38 +0100

    lol

--- Navigation/TurnThread.py
+++ Navigation/TurnThread.py
@@ -68,7 +68,6 @@ class TurnThread():
         debounce=self.wallchecker.compare()
         
         self.dual_motors.softStop()
-        time.sleep(0.7)
         self.dual_motors.turn90(direction, 2)
         time.sleep(0.8)
         
