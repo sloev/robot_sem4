ecea13a20ae1ffdba802f895593056caa20f9932
sloev <johannesgj@gmail.com>
2013-11-07 16:06:49 +0100

    lol

--- Navigation/IterativeNavigation.py
+++ Navigation/IterativeNavigation.py
@@ -32,7 +32,7 @@ class IterativeNavigator():
         self.left=not direction
         self.right=direction
         self.front=2
-        self.maxWidth=35
+        self.maxWidth=34
 
         self.minMaxSetpoint=[5,self.maxWidth,15]
         
