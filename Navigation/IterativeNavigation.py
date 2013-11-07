2758417442a30507e4787c7f71d9fea33a68cc38
sloev <johannesgj@gmail.com>
2013-11-07 16:21:29 +0100

    lol

--- Navigation/IterativeNavigation.py
+++ Navigation/IterativeNavigation.py
@@ -139,7 +139,7 @@ class IterativeNavigator():
         angleV=math.acos(tmp)
         
         if lastWasLeft:
-            direction=self.left
+            direction=self.right
             lengthE=math.cos(angleV)*right
             lengthD=self.maxWidth-lengthE
             lengthC=lengthE-(self.maxWidth/2)
@@ -148,7 +148,7 @@ class IterativeNavigator():
             angleF=(math.pi/2)-angleB
             currentAngle=-(angleF+angleV)
         else:
-            direction=self.right
+            direction=self.left
             lengthD=math.cos(angleV)*left
             lengthE=self.maxWidth-lengthD
             lengthC=lengthD-(self.maxWidth/2)
