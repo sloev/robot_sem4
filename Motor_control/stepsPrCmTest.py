69cd6a1f5fe0bde2c7d672e0070dc33a4bac5e46
sloev <johannesgj@gmail.com>
2013-11-07 14:07:13 +0100

    lol

--- Motor_control/stepsPrCmTest.py
+++ Motor_control/stepsPrCmTest.py
@@ -12,7 +12,7 @@ def main():
     motors=DualMotorController(0x60,0x61)
 
     motors.setOtpParam()
-    tmp=motors.getFullStatus2()
+   # tmp=motors.getFullStatus2()
     motors.setMotorParams(0,1, 2, 2)
     steps=5000
     motors.runInit()
