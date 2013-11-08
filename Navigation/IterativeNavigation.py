08df31e70f30dfa4bce05d5e1ef077761e32196e
sloev <johannesgj@gmail.com>
2013-11-08 09:12:24 +0100

    lol

--- Navigation/IterativeNavigation.py
+++ Navigation/IterativeNavigation.py
@@ -118,6 +118,11 @@ class IterativeNavigator():
     def drive(self,steps):
         self.dual_motors.setPosition(steps, steps)
         while(self.dual_motors.isBusy()):
+            sample=self.ir_sensors.multiChannelReadCm(sensorChannels, 1)
+            walls=self.wallChecker.checkWalls(sample)
+            if(walls[self.left]!=1 or walls[self.right]!=1):#lavet i dag todo
+                self.dual_motors.softStop()
+                break
             self.navigatorStopEvent.wait(0.1)
             
     def driveStraight(self,steps):
