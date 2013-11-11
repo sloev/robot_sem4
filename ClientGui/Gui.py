08cd39cb847ef2d338efec7c60a59b212229099f
sloev <johannesgj@gmail.com>
2013-11-11 22:43:21 +0100

    lol

--- ClientGui/Gui.py
+++ ClientGui/Gui.py
@@ -66,6 +66,7 @@ class MainGui(QtGui.QMainWindow):
 
         if reply == QtGui.QMessageBox.Yes:
             print("old ip and port="+str(self.address))
+            self.address=(ip,port)
             self.initTcpClient()
             print("new ip and port="+str(self.address))
         else:
