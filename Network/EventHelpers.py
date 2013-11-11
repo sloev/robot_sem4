2b60a5ef14602d1ec0658c80794190ddfaa4e375
sloev <johannesgj@gmail.com>
2013-11-11 14:35:21 +0100

    lol

---
+++ Network/EventHelpers.py
@@ -0,0 +1,27 @@
+'''
+Created on Nov 11, 2013
+
+@author: johannes
+'''
+
+class EventHook(object):
+
+    def __init__(self):
+        self.__handlers = []
+
+    def __iadd__(self, handler):
+        self.__handlers.append(handler)
+        return self
+
+    def __isub__(self, handler):
+        self.__handlers.remove(handler)
+        return self
+
+    def fire(self, *args, **keywargs):
+        for handler in self.__handlers:
+            handler(*args, **keywargs)
+
+    def clearObjectHandlers(self, inObject):
+        for theHandler in self.__handlers:
+            if theHandler.im_self == inObject:
+                self -= theHandler
