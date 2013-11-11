9381f0c9f22a2c977f9a2a631de207128b6258fe
danielmachon <danielmachon@live.dk>
2013-11-11 16:50:50 +0100

    dsdsd

--- Navigation/PidTuner.py
+++ Navigation/PidTuner.py
@@ -151,7 +151,7 @@ class PidTuner():
 
             self.printGains()
             'start sampling section'
-            sample=self.ir_sensors.multiChannelReadCm(sensorChannels,3)
+            sample=self.ir_sensors.multiChannelReadCm(sensorChannels,1)
             walls=self.wallChecker.checkWalls(sample)  
             debounce=self.wallChecker.compare()         
             'end of sampling section'
