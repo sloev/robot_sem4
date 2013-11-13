0279f3699b8d2ed89654e3bb34fa3bd155ad1a7a
sloev <johannesgj@gmail.com>
2013-11-13 16:30:10 +0100

    lol

--- Maze/Maze.py
+++ Maze/Maze.py
@@ -29,7 +29,7 @@ class Maze():
         return len(self.table[0])
     
     def get(self,x,y):
-        return self.table[x][y]
+        return self.table[x][y][0]
     
     def getDict(self):
         return self.table
