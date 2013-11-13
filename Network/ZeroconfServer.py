ad0506fe20c5bfd5e139f7b4982acaf831a4b6f9
sloev <johannesgj@gmail.com>
2013-11-13 16:36:14 +0100

    lol

--- Network/ZeroconfServer.py
+++ Network/ZeroconfServer.py
@@ -85,7 +85,8 @@ def printMaze():
             east=random.randint(0.100)>90
             south=random.randint(0.100)>90
             west=random.randint(0.100)>90     
-            value=(((north<<3) or (east <<2)) or (south <<1)) or west       
+            value=(((north<<3) or (east <<2)) or (south <<1)) or west   
+            print str(value)    
             maze.set(x, y, [value])
     print(maze)
     print"finnished"
