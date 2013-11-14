42b5d730e848cc8ab59f019a94a3a0d13f9c903d
danielmachon <danielmachon@live.dk>
2013-11-14 13:35:07 +0100

    fdfdf

--- Navigation/Calibrate.py
+++ Navigation/Calibrate.py
@@ -80,7 +80,7 @@ class Calibrate():
         
         while(doLoop):
             self.motors.setMotorParams(0, 0, 1, 1)
-            self.motors.setPosition(32768, 32768)
+            self.motors.setPosition(10, 10)
             lastSample = sample
             sample = self.sensors.multiChannelReadCm(channels, 3)
             print sample
