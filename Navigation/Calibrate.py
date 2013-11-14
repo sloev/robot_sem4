62fa259062439547d785923be2213677c0f9c566
danielmachon <danielmachon@live.dk>
2013-11-14 14:03:27 +0100

    fdfdff

--- Navigation/Calibrate.py
+++ Navigation/Calibrate.py
@@ -83,7 +83,7 @@ class Calibrate():
             lastSample = sample
             sample = self.sensors.multiChannelReadCm(channels, 10)
             print sample
-            time.sleep(0.001)
+            time.sleep(0.25)
             
         
         self.motors.hardStop()
