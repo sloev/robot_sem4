43da892d3c189259f5f69d841f3e6b504643dced
danielmachon <danielmachon@live.dk>
2013-11-15 13:05:50 +0100

    ghghgh

---
+++ Navigation/Navigator.py
@@ -0,0 +1,33 @@
+'''
+Created on 15/11/2013
+
+@author: danielmachon
+'''
+
+class Navigator:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        self.steps = 0
+        
+    
+    def __call__(self, steps):
+        self.steps = self.steps + steps
+    
+   
+    def resetSteps(self):
+        self.steps = 0
+        
+        
+    def getSteps(self):
+        return self.steps
+    
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
+        
